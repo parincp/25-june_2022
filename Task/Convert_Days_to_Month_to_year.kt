1c74f93fb8558a872923cef1022c7c8c2a9050fa
parincp <102416405+parincp@users.noreply.github.com>
2022-07-22 02:05:02 +0530

    Create Convert_Days_to_Month_to_year.kt

---
+++ Task/Convert_Days_to_Month_to_year.kt
@@ -0,0 +1,15 @@
+fun main(args:Array<String>)
+
+{
+
+    var days = 4570.0
+    var month = 365
+    var totalmonth = days / month
+
+    println("Total Month is : $totalmonth")
+
+    var totalyear = totalmonth / 12
+
+    println("Total Year is : $totalyear")
+
+}
