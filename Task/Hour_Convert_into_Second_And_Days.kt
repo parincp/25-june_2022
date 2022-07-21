903e8801ec088271755fbacfd1d8c1edc97e6772
parincp <102416405+parincp@users.noreply.github.com>
2022-07-22 02:05:20 +0530

    Create Hour_Convert_into_Second_And_Days.kt

---
+++ Task/Hour_Convert_into_Second_And_Days.kt
@@ -0,0 +1,11 @@
+fun main(args:Array<String>)
+
+{
+    var totalhours = 45798
+    var second = totalhours*3600
+    print ("Total 45798 Hours =  $second")
+    println(" Seconds")
+    var day = totalhours/24
+    print("Total 45798 hour = $day ")
+    println("Days")
+}
