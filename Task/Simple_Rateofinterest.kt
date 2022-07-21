d935c2c5af837079d2259cc74c56eb42ae7addc4
parincp <102416405+parincp@users.noreply.github.com>
2022-07-22 02:05:38 +0530

    Create Simple_Rateofinterest.kt

---
+++ Task/Simple_Rateofinterest.kt
@@ -0,0 +1,9 @@
+//fun main(args:Array<String>)
+//{
+   // var days:Int = 10
+    //var interestrate:Float = 15
+    //var principalamt:Int = 10000
+    //var interestpayable = principalamt * interestrate * days
+
+    //println(interestpayable)
+//}
