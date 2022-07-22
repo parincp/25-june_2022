51e6acd85a6bafc3653eed68aa5771fb4e9029e7
parincp <102416405+parincp@users.noreply.github.com>
2022-07-23 01:10:10 +0530

    Create print_an_Integer_2.kt

---
+++ Module-2/print_an_Integer_2.kt
@@ -0,0 +1,15 @@
+import java.util.Scanner
+
+fun main(args:Array<String>)
+{
+    var num1:Int
+    var num2:Int
+    var sc = Scanner(System.`in`)
+    println("Please Enter 1st Integer Value : ")
+    num1 = sc.nextInt()
+    println("Please Enter 2nd Integer Value : ")
+    num2 = sc.nextInt()
+
+    println(" First Integer Values is  : $num1")
+    println(" Second Integer Values is  : $num2")
+}
